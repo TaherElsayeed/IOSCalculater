4c1a2a36787b466b85864e71ef88226208df300d
Taher Alsayeed <taheralsayeed@tahers-macbook-pro.local>
2018-11-28 15:44:06 +0200

    Initial Commit

---
+++ Calcullater/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calcullater
+//
+//  Created by Taher Alsayeed on 28/11/2018.
+//  Copyright © 2018 Taher Elsayeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
